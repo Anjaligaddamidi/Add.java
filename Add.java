41c791ce7003c38af2987f7e0ff0e204b3a17f6b
Anjaligaddamidi <149222107+Anjaligaddamidi@users.noreply.github.com>
2024-04-04 12:59:08 +0530

    ADD

---
+++ Add.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Add
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("enter the values of a and b:");
+int a=sc.nextInt();
+int b=sc.nextInt();
+System.out.println("addition of"+a+"+"+b+"is"+(a+b));
+}
+}
